28a6de3272c7513ff2b4c96e973a5561292cead0
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2011-10-22 02:48:14 -0200

    Initial structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='pycket',
+      version=version,
+      description="Redis sessions for Tornado",
+      long_description="""\
+Redis user sessions to use with Tornado server""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='pycket redis tornado session python',
+      author='Diogo Baeder',
+      author_email='desenvolvedor@diogobaeder.com.br',
+      url='',
+      license='BSD 2-Clause',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
