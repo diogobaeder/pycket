0b23bf6628c42b835dbba55466b6d0752e2528f7
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2011-10-23 01:24:48 -0200

    Notification documentation

--- pycket/notification.py
+++ pycket/notification.py
@@ -1,3 +1,10 @@
+'''
+This module is the same as the sessions module, except that NotificationMixin
+sets a "notifications" property instead a "session" one, and that the
+NotificationManager ("notifications") gets an object only once, and deletes it
+from the database after retrieving
+'''
+
 from pycket.session import SessionManager, SessionMixin
 
 
@@ -5,6 +12,11 @@ class NotificationManager(SessionManager):
     DB_NAME = 'pycket_notifications'
 
     def get(self, name):
+        '''
+        Retrieves the object with "name", like with SessionManager.get(), but
+        removes the object from the database after retrieval, so that it can be
+        retrieved only once
+        '''
         session_object = super(NotificationManager, self).get(name)
         if session_object is not None:
             self.delete(name)
